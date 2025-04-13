2f0a6ce00651a5f944134a0100a4ca438ee8fb46
Althaf Subair <althafsubair3@gmail.com>
2025-04-13 20:24:37 +0530

    updated limit and cors

--- server/index.js
+++ server/index.js
@@ -13,9 +13,14 @@ const app = express();
 
 job.start();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+app.use(cors({
+    origin: '*', // You can also restrict to your frontend URL for more security
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 
 app.use('/api/auth', authRouter);
 app.use('/api/books', bookRouter);
